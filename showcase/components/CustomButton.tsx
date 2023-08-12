410e251fbdd1b4e8cc5fbf50e3fd9007f0574715
Dima <dimakurinnoy8@gmail.com>
2023-08-12 23:10:41 +0200

    create hero section

--- showcase/components/CustomButton.tsx
+++ showcase/components/CustomButton.tsx
@@ -2,11 +2,11 @@
 
 import { CustomButtonProps } from '@/types';
 
-const CustomButton = ({ title, containerStiles, handleClick }: CustomButtonProps) => {
+const CustomButton = ({ title, containerStiles, handleClick ,btnType}: CustomButtonProps) => {
   return (
     <button
       disabled={false}
-      type={'button'}
+      type={btnType || 'button'}
       className={`custom-btn ${containerStiles}`}
       onClick={handleClick}>
       <span className={`flex-1`}>{title}</span>
--- showcase/types/index.ts
+++ showcase/types/index.ts
@@ -1,7 +1,8 @@
-import { MouseEventHandler } from "react";
+import { MouseEventHandler } from 'react';
 
-export interface CustomButtonProps{
-    title: string;
-    containerStiles?: string
-    handleClick?:MouseEventHandler<HTMLButtonElement>
+export interface CustomButtonProps {
+  title: string;
+  containerStiles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: 'button' | 'submit';
 }
